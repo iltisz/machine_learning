7adb6edf8d62cb304b0c45840ecc7273db0e77b9
Hendrik Jansen <hendrik.jansen@WindGuard.local>
2019-02-26 15:45:32 +0100

    forth commit

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,10 @@
 ## begin #2 chapter
 import pandas as pd
 import quandl
+import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
@@ -10,7 +14,26 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-print(df.head())
+#print(df.head())
 
 
 ## begin #3 chapter
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+#print (df.head())
+
+## begin #4 chapter
+
+#normaly label are capital X
+#features are lower case y
+
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
